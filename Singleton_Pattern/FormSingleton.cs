a8becc5f77b6089c8a0746719ac047da56879e25
Serhat YAMAN <serhatyaman@hotmail.com.tr>
2021-10-24 23:05:40 +0300

    Add project files.

---
+++ Singleton_Pattern/FormSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton_Pattern
+{
+    public partial class FormSingleton : Form
+    {
+        public FormSingleton()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSingleton_Load(object sender, EventArgs e)
+        {
+            //You can not create an instance by using new keyword because we made our constructor private.
+
+            SingletonExample singleton = SingletonExample.GetInstance();
+            SingletonExample singleton1 = SingletonExample.GetInstance();
+
+            MessageBox.Show($"{singleton.GetHashCode()} - {singleton1.GetHashCode()}");
+
+            #region Output
+            //Output
+            //30015890 - 30015890 
+            #endregion
+
+            //Which means we used the same "SingletonExample" instance because we already created an instance named "singleton" before. 
+        }
+    }
+}
---
+++ Singleton_Pattern/SingletonExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public partial class SingletonExample
+    {
+        private SingletonExample() { } //Private constructor.
+        private static SingletonExample _instance;
+
+        public static SingletonExample GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SingletonExample();
+            }
+            return _instance;
+        }
+    }
+}
